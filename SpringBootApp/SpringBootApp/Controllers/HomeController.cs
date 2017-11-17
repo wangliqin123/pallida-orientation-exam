9a9d0f7d49953449cfe297203574b86bb48b9a5b
Lorand <szilagyi.lori@gmail.com>
2017-11-17 09:59:08 +0100

    feat(SpringBootApp): filled folders with template classes

---
+++ SpringBootApp/SpringBootApp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpringBootApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("/search")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
